236d5779b952c20a798c39ddd5d38e4e068f5289
Axel Benjamín Alvarez Alcaraz <axelalvarez0200@gmail.com>
2025-07-24 11:07:46 -0300

    Errores solucionados excepto por el primer test en MedicoRepositoryTest

--- src/main/java/med/voll/api/domain/consulta/Consulta.java
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -1,6 +1,8 @@
 package med.voll.api.domain.consulta;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -32,4 +34,9 @@ public class Consulta {
 
     private LocalDateTime fecha;
 
+    // Asegúrate de tener este campo
+    private String motivoCancelamiento;
+
+    public Consulta(Object o, Medico medico, Paciente paciente, @NotNull @Future LocalDateTime fecha) {
+    }
 }
